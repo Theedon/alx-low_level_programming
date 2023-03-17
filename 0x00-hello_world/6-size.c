631f943debfa63396710f3033f322555fafbf870
Theedon <faniyantoyin@gmail.com>
2023-03-17 06:09:09 +0000

    corrections for 5 and 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 /**
- *entry point for main
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -10,4 +13,4 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
-}       
+}
