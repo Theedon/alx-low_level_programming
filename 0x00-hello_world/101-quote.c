a0fbeb2ab5ecc93eb6fd5daf32134112a934182c
Theedon <faniyantoyin@gmail.com>
2023-03-18 17:51:39 +0000

    add advanced task 7 in hello world directory

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main- entry point
+ *
+ *Return: exit point
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 55);
+	return (1);
+}
