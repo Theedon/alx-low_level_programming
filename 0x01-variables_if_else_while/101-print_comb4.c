53bc1dd6feb0a0e7ff78911661f7980f6a4917d2
Theedon <faniyantoyin@gmail.com>
2023-03-19 18:32:24 +0000

    coorection for task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -27,8 +27,14 @@ int main(void)
 					putchar(i % 10 + '0');
 					putchar(j % 10 + '0');
 					putchar(k % 10 + '0');
-					putchar(',');
-					putchar(' ');
+					if (i == 7 && j == 8 && k == 9)
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
