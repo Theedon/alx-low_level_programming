4f33016e4f0e23d6cf36580fd26c29360484a02a
Theedon <faniyantoyin@gmail.com>
2023-03-22 02:35:18 +0000

    add header file for function prototypes

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ *
+ *File: main.h
+ *Author: Theedon
+ *Description: Header file containing declarations for all functionsused in the
+ 0x02-functions_nested_loops directory.
+*/
+
+int _putchar(char c);
+
+
+#endif
