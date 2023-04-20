985c5f1fac5fa8a166bf8e6650d60c02de4675ac
Theedon <faniyantoyin@gmail.com>
2023-04-20 10:43:38 +0000

    correctio for task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	void (*p)(int) = action;
 	size_t i;
 
+	if (array == NULL || size == 0 || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
-		p(array[i]);
+		action(array[i]);
 	}
 }
